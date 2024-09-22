bc94d90d59ef24ba255bb87b06144a23dbfb8dc6
RussellBentley <russell.w.bentley@icloud.com>
2024-09-21 21:20:20 -0400

    Add new stencil struct

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,4 @@ pub mod apply_iter;
 pub mod domain;
 pub mod linear_stencil;
 pub mod naive_solver;
+pub mod stencil;
---
+++ src/stencil.rs
@@ -0,0 +1,61 @@
+pub struct Stencil<FloatType, Operation, const DIMENSION: usize, const NEIGHBORHOOD_SIZE: usize>
+where
+    Operation: Fn(&[FloatType; NEIGHBORHOOD_SIZE]) -> FloatType,
+{
+    operation: Operation,
+    offsets: [[i32; DIMENSION]; NEIGHBORHOOD_SIZE],
+    float_type: std::marker::PhantomData<FloatType>,
+}
+
+impl<FloatType, Operation, const DIMENSION: usize, const NEIGHBORHOOD_SIZE: usize>
+    Stencil<FloatType, Operation, DIMENSION, NEIGHBORHOOD_SIZE>
+where
+    Operation: Fn(&[FloatType; NEIGHBORHOOD_SIZE]) -> FloatType,
+    FloatType: num::Float,
+{
+    pub fn new(offsets: [[i32; DIMENSION]; NEIGHBORHOOD_SIZE], operation: Operation) -> Self {
+        Stencil {
+            offsets,
+            operation,
+            float_type: std::marker::PhantomData,
+        }
+    }
+
+    /// For linear stencils, we can extract the weight for a neighbor
+    /// by passing in 1.0 for that neighbor and 0.0 for the others.
+    pub fn extract_weights(&self) -> [FloatType; NEIGHBORHOOD_SIZE] {
+        let mut weights = [FloatType::zero(); NEIGHBORHOOD_SIZE];
+        let mut arg_buffer = [FloatType::zero(); NEIGHBORHOOD_SIZE];
+        for n in 0..NEIGHBORHOOD_SIZE {
+            arg_buffer[n] = FloatType::one();
+            weights[n] = (self.operation)(&arg_buffer);
+            arg_buffer[n] = FloatType::zero();
+        }
+        weights
+    }
+}
+
+#[cfg(test)]
+mod unit_tests {
+    use super::*;
+    use float_cmp::assert_approx_eq;
+
+    #[test]
+    fn extract_weights() {
+        {
+            let s = Stencil::new([[1]], |args: &[f32; 1]| 2.0 * args[0]);
+            let w = s.extract_weights()[0];
+            assert_approx_eq!(f32, w, 2.0);
+        }
+
+        {
+            let s = Stencil::new([[1], [2], [3]], |args: &[f32; 3]| {
+                2.0 * args[0] + 3.0 * args[1] + 5.0 * args[2]
+            });
+            let w = s.extract_weights();
+            assert_approx_eq!(f32, w[0], 2.0, ulps = 1);
+            assert_approx_eq!(f32, w[1], 3.0, ulps = 1);
+            assert_approx_eq!(f32, w[2], 5.0, ulps = 1);
+        }
+    }
+}
