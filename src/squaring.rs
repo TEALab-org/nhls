a8453e894d6c4e133f996d8f584ef27f0fe4f823
RussellBentley <russell.w.bentley@icloud.com>
2024-11-06 14:52:24 -0500

    Add parallelized vector squaring

--- src/squaring.rs
+++ src/squaring.rs
@@ -1,31 +1,70 @@
+use num_traits::{Num, One};
+use rayon::prelude::*;
+
 pub fn log2ceil(i: usize) -> usize {
     (usize::BITS - i.leading_zeros()) as usize
 }
 
+// Reference implementation for integers
 pub fn repeated_square(exp0: usize, i: usize) -> usize {
     let mut exp = exp0;
     let mut squares = i;
     let mut result = 1;
     let n = log2ceil(exp0);
-    println!(
-        "repeated, n: {}, exp: {}, result: {}, squares: {}",
-        n, exp, result, squares
-    );
     for _ in 0..n {
         if exp % 2 == 1 {
             result *= squares;
             exp -= 1;
-            println!(
-                "  - odd exp, result: {}, squares: {}, exp: {}",
-                result, squares, exp
-            );
         }
         exp /= 2;
         squares *= squares;
-        println!(" * exp: {}, result: {}, squares: {}", exp, result, squares);
     }
 
     result
+}
+
+// Parallelized vector squaring
+pub fn repeated_square_slice<NumType: Num + Copy + Send + std::marker::Sized>(
+    exp0: usize,
+    data: &mut [NumType],
+    result_buffer: &mut [NumType],
+    chunk_size: usize,
+) {
+    debug_assert!(data.len() == result_buffer.len());
+    let mut exp = exp0;
+
+    // Result buffer starts as 1
+    result_buffer
+        .par_chunks_mut(chunk_size)
+        .for_each(|xs: &mut [NumType]| {
+            for x in xs {
+                *x = NumType::one();
+            }
+        });
+
+    let n = log2ceil(exp0);
+    for _ in 0..n {
+        if exp % 2 == 1 {
+            result_buffer
+                .par_chunks_mut(chunk_size)
+                .zip(data.par_chunks_mut(chunk_size))
+                .for_each(|(rs, ds)| {
+                    for (r, d) in rs.iter_mut().zip(ds.iter_mut()) {
+                        *r = *r * *d;
+                    }
+                });
+
+            //result *= data;
+            exp -= 1;
+        }
+        exp /= 2;
+        data.par_chunks_mut(chunk_size)
+            .for_each(|xs: &mut [NumType]| {
+                for x in xs {
+                    *x = *x * *x;
+                }
+            });
+    }
 }
 
 #[cfg(test)]
@@ -51,5 +90,25 @@ mod unit_tests {
         assert_eq!(repeated_square(4, 2), 16);
         assert_eq!(repeated_square(5, 2), 32);
         assert_eq!(repeated_square(9, 3), 19683);
+    }
+
+    #[test]
+    fn repeated_square_slice_test() {
+        {
+            let mut data = vec![1, 2, 3, 4, 5];
+            let mut buffer = vec![0; 5];
+            repeated_square_slice(5, &mut data, &mut buffer, 1);
+            for (i, x) in buffer.iter().enumerate() {
+                assert_eq!(*x, (i + 1).pow(5));
+            }
+        }
+        {
+            let mut data = vec![1, 2, 3, 4, 5];
+            let mut buffer = vec![0; 5];
+            repeated_square_slice(5, &mut data, &mut buffer, 100);
+            for (i, x) in buffer.iter().enumerate() {
+                assert_eq!(*x, (i + 1).pow(5));
+            }
+        }
     }
 }
