97ffb1c483902a67a60ebf585717b79ac20a6e98
RussellBentley <russell.w.bentley@icloud.com>
2024-11-14 01:03:21 -0500

    Feel pretty good about periodic plan

--- src/solver/periodic_plan.rs
+++ src/solver/periodic_plan.rs
@@ -90,11 +90,19 @@ where
         let convolution = self.convolution_map.get(&key).unwrap();
         let fft_plan = self.fft_plan_library.get_plan(size);
 
-        fft_plan.forward_plan.r2c(input, complex_buffer).unwrap();
+        // fftw bindings expect slices of specific size
+        let n_r = real_buffer_size(&size);
+        let n_c = complex_buffer_size(&size);
+        fft_plan
+            .forward_plan
+            .r2c(&mut input[0..n_r], &mut complex_buffer[0..n_c])
+            .unwrap();
         par_slice::multiply_by(complex_buffer, convolution.as_slice(), chunk_size);
-        fft_plan.backward_plan.c2r(complex_buffer, output).unwrap();
-        let n = real_buffer_size(&size);
-        par_slice::div(output, n as f32, chunk_size);
+        fft_plan
+            .backward_plan
+            .c2r(&mut complex_buffer[0..n_c], &mut output[0..n_r])
+            .unwrap();
+        par_slice::div(output, n_r as f32, chunk_size);
     }
 
     fn new_convolution(
@@ -147,7 +155,7 @@ mod unit_tests {
     use nalgebra::vector;
 
     #[test]
-    fn test_1d() {
+    fn test_1d_simple() {
         let chunk_size = 1;
         let stencil = Stencil::new([[0]], |args: &[f32; 1]| args[0]);
         let max_size = vector![100];
@@ -161,6 +169,7 @@ mod unit_tests {
             for x in input_x.as_slice_mut() {
                 *x = 1.0f32;
             }
+            let input_copy = input_x.clone();
             let mut complex_buffer = fftw::array::AlignedVec::new(100);
             let mut result_buffer = fftw::array::AlignedVec::new(100);
             plan_library.apply(
@@ -168,13 +177,13 @@ mod unit_tests {
                 10,
                 &mut input_x,
                 &mut result_buffer,
-                &mut complex_buffer[0..complex_buffer_size],
+                &mut complex_buffer,
                 chunk_size,
             );
-            let mut input_x = fftw::array::AlignedVec::new(100);
-            for x in input_x.as_slice_mut() {
-                *x = 1.0f32;
+            for x in &result_buffer[0..100] {
+                assert_approx_eq!(f32, *x, 1.0);
             }
+            assert_eq!(input_x.as_slice(), input_copy.as_slice());
         }
 
         {
@@ -182,19 +191,179 @@ mod unit_tests {
             for x in input_x.as_slice_mut() {
                 *x = 1.0f32;
             }
-            let mut complex_buffer = fftw::array::AlignedVec::new(100);
-            let mut result_buffer = fftw::array::AlignedVec::new(100);
+            let input_copy = input_x.clone();
+            let mut complex_buffer = fftw::array::AlignedVec::new(200);
+            let mut result_buffer = fftw::array::AlignedVec::new(200);
             plan_library.apply(
                 max_size,
                 20,
                 &mut input_x,
                 &mut result_buffer,
-                &mut complex_buffer[0..complex_buffer_size],
+                &mut complex_buffer,
                 chunk_size,
             );
-            for x in result_buffer.as_slice() {
+            for x in &result_buffer[0..100] {
                 assert_approx_eq!(f32, *x, 1.0);
             }
+            assert_eq!(input_x.as_slice(), input_copy.as_slice());
         }
+    }
+
+    #[test]
+    fn test_2d_simple() {
+        let chunk_size = 1;
+        let stencil = Stencil::new([[0, 0]], |args: &[f32; 1]| args[0]);
+        let max_size = vector![50, 50];
+        let real_buffer_size = crate::solver::fft_plan::real_buffer_size(&max_size);
+        let complex_buffer_size = crate::solver::fft_plan::complex_buffer_size(&max_size);
+        let mut plan_library = PeriodicPlanLibrary::new(&max_size, &stencil);
+
+        let mut input_x = fftw::array::AlignedVec::new(real_buffer_size);
+        for x in input_x.as_slice_mut() {
+            *x = 1.0f32;
+        }
+        let input_copy = input_x.clone();
+        let mut complex_buffer = fftw::array::AlignedVec::new(complex_buffer_size + 10);
+        let mut result_buffer = fftw::array::AlignedVec::new(real_buffer_size + 14);
+        plan_library.apply(
+            max_size,
+            10,
+            &mut input_x,
+            &mut result_buffer,
+            &mut complex_buffer,
+            chunk_size,
+        );
+        for x in &result_buffer[0..100] {
+            assert_approx_eq!(f32, *x, 1.0);
+        }
+        assert_eq!(input_x.as_slice(), input_copy.as_slice());
+    }
+
+    #[test]
+    fn test_2d_less_simple() {
+        let chunk_size = 1;
+        let stencil = Stencil::new(
+            [[0, -1], [0, 1], [1, 0], [-1, 0], [0, 0]],
+            |args: &[f32; 5]| {
+                debug_assert_eq!(args.len(), 5);
+                let mut r = 0.0;
+                for a in args {
+                    r += a / 5.0;
+                }
+                r
+            },
+        );
+        assert_eq!(stencil.apply(&[1.0; 5]), 1.0);
+        println!("{:?}", stencil.extract_weights());
+        let max_size = vector![50, 50];
+        let real_buffer_size = crate::solver::fft_plan::real_buffer_size(&max_size);
+        let complex_buffer_size = crate::solver::fft_plan::complex_buffer_size(&max_size);
+        let mut plan_library = PeriodicPlanLibrary::new(&max_size, &stencil);
+
+        let mut input_x = fftw::array::AlignedVec::new(real_buffer_size + 4);
+        for x in input_x.as_slice_mut() {
+            *x = 1.0f32;
+        }
+        let input_copy = input_x.clone();
+        let mut complex_buffer = fftw::array::AlignedVec::new(complex_buffer_size + 10);
+        let mut result_buffer = fftw::array::AlignedVec::new(real_buffer_size + 14);
+        plan_library.apply(
+            max_size,
+            1,
+            &mut input_x,
+            &mut result_buffer,
+            &mut complex_buffer,
+            chunk_size,
+        );
+        for x in &result_buffer[0..real_buffer_size] {
+            assert_approx_eq!(f32, *x, 1.0);
+        }
+        assert_eq!(input_x.as_slice(), input_copy.as_slice());
+    }
+
+    #[test]
+    fn test_1d_less_simple() {
+        let chunk_size = 1;
+        let stencil = Stencil::new([[-1], [1], [0]], |args: &[f32; 3]| {
+            debug_assert_eq!(args.len(), 3);
+            let mut r = 0.0;
+            for a in args {
+                r += a / 3.0
+            }
+            r
+        });
+        let max_size = vector![100];
+        let real_buffer_size = crate::solver::fft_plan::real_buffer_size(&max_size);
+        let complex_buffer_size = crate::solver::fft_plan::complex_buffer_size(&max_size);
+        let mut plan_library = PeriodicPlanLibrary::new(&max_size, &stencil);
+
+        let mut input_x = fftw::array::AlignedVec::new(real_buffer_size + 34);
+        for x in input_x.as_slice_mut() {
+            *x = 1.0f32;
+        }
+        let input_copy = input_x.clone();
+        let mut complex_buffer = fftw::array::AlignedVec::new(complex_buffer_size + 1);
+        let mut result_buffer = fftw::array::AlignedVec::new(real_buffer_size + 13);
+        plan_library.apply(
+            max_size,
+            10,
+            &mut input_x,
+            &mut result_buffer,
+            &mut complex_buffer,
+            chunk_size,
+        );
+        for x in &result_buffer[0..real_buffer_size] {
+            assert_approx_eq!(f32, *x, 1.0);
+        }
+        assert_eq!(input_x.as_slice(), input_copy.as_slice());
+    }
+
+    #[test]
+    fn test_3d() {
+        let chunk_size = 1;
+        let stencil = Stencil::new(
+            [
+                [0, 0, -2],
+                [4, 5, 3],
+                [0, -1, 0],
+                [0, 1, 0],
+                [1, 0, 0],
+                [-1, 0, 4],
+                [0, 0, 0],
+            ],
+            |args: &[f32; 7]| {
+                let mut r = 0.0;
+                for a in args {
+                    r += a / 7.0;
+                }
+                r
+            },
+        );
+        assert_eq!(stencil.apply(&[1.0; 7]), 1.0);
+        println!("{:?}", stencil.extract_weights());
+        let max_size = vector![20, 20, 20];
+        let real_buffer_size = crate::solver::fft_plan::real_buffer_size(&max_size);
+        let complex_buffer_size = crate::solver::fft_plan::complex_buffer_size(&max_size);
+        let mut plan_library = PeriodicPlanLibrary::new(&max_size, &stencil);
+
+        let mut input_x = fftw::array::AlignedVec::new(real_buffer_size + 4);
+        for x in input_x.as_slice_mut() {
+            *x = 1.0f32;
+        }
+        let input_copy = input_x.clone();
+        let mut complex_buffer = fftw::array::AlignedVec::new(complex_buffer_size + 10);
+        let mut result_buffer = fftw::array::AlignedVec::new(real_buffer_size + 14);
+        plan_library.apply(
+            max_size,
+            20,
+            &mut input_x,
+            &mut result_buffer,
+            &mut complex_buffer,
+            chunk_size,
+        );
+        for x in &result_buffer[0..real_buffer_size] {
+            assert_approx_eq!(f32, *x, 1.0);
+        }
+        assert_eq!(input_x.as_slice(), input_copy.as_slice());
     }
 }
