0dfa944cf9d6f5e6015025f155f6745a1d35fbc1
RussellBentley <russell.w.bentley@icloud.com>
2024-12-20 15:32:47 -0500

    Add AABB::shrink and try_fftsolve function

--- src/decomposition/mod.rs
+++ src/decomposition/mod.rs
@@ -35,3 +35,31 @@ pub fn recursive_solve(args: FFTSolveArgs, p: FFTParams, level: usize) {
     println!("RS: args: {:?}, p: {:?}", args, p);
 }
 */
+use crate::util::*;
+
+pub struct FFTSolveParams<const DIMENSION: usize> {
+    pub slopes: Bounds<DIMENSION>,
+    pub cutoff: i32,
+    pub ratio: f64,
+}
+
+pub struct FFTSolve<const DIMENSION: usize> {
+    pub solve_region: AABB<DIMENSION>,
+    pub steps: i32,
+}
+
+pub fn try_fftsolve<const DIMENSION: usize>(
+    bounds: AABB<DIMENSION>,
+    params: FFTSolveParams<DIMENSION>,
+) -> Option<FFTSolve<DIMENSION>> {
+    if bounds.min_size_len() <= params.cutoff {
+        return None;
+    }
+
+    let (steps, solve_region) = bounds.shrink(params.ratio, params.slopes);
+
+    Some(FFTSolve {
+        solve_region,
+        steps,
+    })
+}
