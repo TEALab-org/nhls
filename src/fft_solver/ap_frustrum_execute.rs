3d85c3699f6915004e11b74afb620867d54b83b1
RussellBentley <russell.w.bentley@icloud.com>
2025-01-08 23:21:20 -0500

    Add ap_frustrum and frustrum utils

--- src/fft_solver/ap_frustrum_execute.rs
+++ src/fft_solver/ap_frustrum_execute.rs
@@ -2,17 +2,19 @@ use crate::domain::*;
 use crate::fft_solver::*;
 use crate::util::*;
 
-struct StackFrame {
+struct StackFrame<'a, const GRID_DIMENSION: usize> {
     op: NodeId,
+    domain_id: DomainId,
+    domain: &'a SliceDomain<'a, GRID_DIMENSION>,
 }
 
 pub struct APFrustrumPlanRunner<'a, const GRID_DIMENSION: usize> {
     complex_buffer: AlignedVec<c64>,
-    real_buffer: AlignedVec<f64>,
     chunk_size: usize,
     plan: ap_frustrum_plan::APFrustrumPlan<GRID_DIMENSION>,
     op_store: &'a ConvolutionStore,
-    stack: Vec<StackFrame>,
+    domain_stack: &'a DomainStack<'a, GRID_DIMENSION>,
+    stack: Vec<StackFrame<'a, GRID_DIMENSION>>,
 }
 
 impl<'a, const GRID_DIMENSION: usize> APFrustrumPlanRunner<'a, GRID_DIMENSION> {
@@ -20,5 +22,6 @@ impl<'a, const GRID_DIMENSION: usize> APFrustrumPlanRunner<'a, GRID_DIMENSION> {
         solve_input_domain: &DomainType,
         solve_output_domain: &mut DomainType,
     ) {
+        loop {}
     }
 }
