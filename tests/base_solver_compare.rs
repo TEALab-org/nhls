c38a047dc13383a9e5eba2b6c938be4ef43f260d
RussellBentley <russell.w.bentley@icloud.com>
2025-02-01 15:48:00 -0500

    add ap compare tests

--- tests/base_solver_compare.rs
+++ tests/base_solver_compare.rs
@@ -1,12 +1,10 @@
+use float_cmp::assert_approx_eq;
 use nhls::domain::*;
 use nhls::fft_solver::*;
-use nhls::init;
+use nhls::init::*;
 use nhls::solver::*;
 use nhls::stencil::*;
 use nhls::util::*;
-
-use float_cmp::assert_approx_eq;
-use nalgebra::matrix;
 
 #[test]
 fn heat_1d_p_compare() {
@@ -28,15 +26,8 @@ fn heat_1d_p_compare() {
     let mut fft_output_domain = OwnedDomain::new(grid_bound);
 
     // Fill in with IC values (use normal dist for spike in the middle)
-    let n_f = buffer_size as f64;
-    let sigma_sq: f64 = (n_f / 25.0) * (n_f / 25.0);
-    let ic_gen = |world_coord: Coord<1>| {
-        let x = (world_coord[0] as f64) - (n_f / 2.0);
-        let exp = -x * x / (2.0 * sigma_sq);
-        exp.exp()
-    };
-    direct_input_domain.par_set_values(ic_gen, chunk_size);
-    fft_input_domain.par_set_values(ic_gen, chunk_size);
+    normal_ic_1d(&mut direct_input_domain, chunk_size);
+    normal_ic_1d(&mut fft_input_domain, chunk_size);
 
     let plan_type = PlanType::Estimate;
     let mut periodic_solver = PeriodicSolver::create(
@@ -86,8 +77,8 @@ fn heat_2d_p_compare() {
     let mut fft_input_domain = OwnedDomain::new(grid_bound);
     let mut fft_output_domain = OwnedDomain::new(grid_bound);
 
-    init::normal_ic_2d(&mut direct_input_domain, chunk_size);
-    init::normal_ic_2d(&mut fft_input_domain, chunk_size);
+    normal_ic_2d(&mut direct_input_domain, chunk_size);
+    normal_ic_2d(&mut fft_input_domain, chunk_size);
 
     let plan_type = PlanType::Estimate;
     let mut periodic_solver = PeriodicSolver::create(
@@ -174,175 +165,3 @@ fn periodic_compare() {
         }
     }
 }
-/*
-#[test]
-fn heat_1d_ap_compare() {
-    // Grid size
-    let grid_bound = AABB::new(matrix![0, 999]);
-
-    let n_steps = 400;
-
-    let chunk_size = 100;
-
-    let stencil = nhls::standard_stencils::heat_1d(1.0, 1.0, 0.5);
-
-    // Create domains
-    let buffer_size = grid_bound.buffer_size();
-    let mut direct_input_domain = OwnedDomain::new(grid_bound);
-    let mut direct_output_domain = OwnedDomain::new(grid_bound);
-
-    let mut fft_input_domain = OwnedDomain::new(grid_bound);
-    let mut fft_output_domain = OwnedDomain::new(grid_bound);
-
-    // Create BC
-    let bc = ConstantCheck::new(1.0, grid_bound);
-
-    // Create AP Solver
-    let cutoff = 40;
-    let ratio = 0.5;
-    let mut solver = APSolver::new(
-        &bc,
-        &stencil,
-        cutoff,
-        ratio,
-        PlanType::Estimate,
-        chunk_size,
-    );
-
-    solver.loop_solve(&mut fft_input_domain, &mut fft_output_domain, n_steps);
-
-    box_apply(
-        &bc,
-        &stencil,
-        &mut direct_input_domain,
-        &mut direct_output_domain,
-        n_steps,
-        chunk_size,
-    );
-
-    for i in 0..buffer_size {
-        assert_approx_eq!(
-            f64,
-            fft_output_domain.buffer()[i],
-            direct_output_domain.buffer()[i],
-            epsilon = 0.00000000000001
-        );
-    }
-}
-
-#[test]
-fn heat_2d_ap_compare() {
-    // Grid size
-    let grid_bound = AABB::new(matrix![0, 50; 0, 50;]);
-
-    let n_steps = 100;
-
-    let chunk_size = 100;
-
-    let stencil = nhls::standard_stencils::heat_2d(1.0, 1.0, 1.0, 0.2, 0.2);
-
-    // Create domains
-    let buffer_size = grid_bound.buffer_size();
-    let mut direct_input_domain = OwnedDomain::new(grid_bound);
-    let mut direct_output_domain = OwnedDomain::new(grid_bound);
-
-    let mut fft_input_domain = OwnedDomain::new(grid_bound);
-    let mut fft_output_domain = OwnedDomain::new(grid_bound);
-
-    // Create BC
-    let bc = ConstantCheck::new(1.0, grid_bound);
-
-    // Create AP Solver
-    let cutoff = 40;
-    let ratio = 0.5;
-    let mut solver = APSolver::new(
-        &bc,
-        &stencil,
-        cutoff,
-        ratio,
-        &grid_bound,
-        PlanType::Estimate,
-        chunk_size,
-    );
-
-    solver.loop_solve(&mut fft_input_domain, &mut fft_output_domain, n_steps);
-
-    box_apply(
-        &bc,
-        &stencil,
-        &mut direct_input_domain,
-        &mut direct_output_domain,
-        n_steps,
-        chunk_size,
-    );
-
-    for i in 0..buffer_size {
-        assert_approx_eq!(
-            f64,
-            fft_output_domain.buffer()[i],
-            direct_output_domain.buffer()[i],
-            epsilon = 0.00000000000001
-        );
-    }
-}
-
-#[test]
-fn heat_3d_ap_compare() {
-    // Grid size
-    let grid_bound = AABB::new(matrix![0, 20; 0, 20;0, 20]);
-
-    let n_steps = 100;
-
-    let chunk_size = 100;
-
-    let stencil =
-        nhls::standard_stencils::heat_3d(1.0, 1.0, 1.0, 1.0, 0.1, 0.1, 0.1);
-    let weights = stencil.extract_weights();
-    let s: f64 = weights.iter().sum();
-    println!("weights: {:?}, s: {}", weights, s);
-
-    // Create domains
-    let buffer_size = grid_bound.buffer_size();
-    let mut direct_input_domain = OwnedDomain::new(grid_bound);
-    let mut direct_output_domain = OwnedDomain::new(grid_bound);
-
-    let mut fft_input_domain = OwnedDomain::new(grid_bound);
-    let mut fft_output_domain = OwnedDomain::new(grid_bound);
-
-    // Create BC
-    let bc = ConstantCheck::new(1.0, grid_bound);
-
-    // Create AP Solver
-    let cutoff = 40;
-    let ratio = 0.5;
-    let mut solver = APSolver::new(
-        &bc,
-        &stencil,
-        cutoff,
-        ratio,
-        &grid_bound,
-        PlanType::Estimate,
-        chunk_size,
-    );
-
-    solver.loop_solve(&mut fft_input_domain, &mut fft_output_domain, n_steps);
-
-    box_apply(
-        &bc,
-        &stencil,
-        &mut direct_input_domain,
-        &mut direct_output_domain,
-        n_steps,
-        chunk_size,
-    );
-
-    for i in 0..buffer_size {
-        assert_approx_eq!(
-            f64,
-            fft_output_domain.buffer()[i],
-            direct_output_domain.buffer()[i],
-            epsilon = 0.00000000000001
-        );
-    }
-}
-*/
