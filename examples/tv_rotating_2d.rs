be0ed982b3e264896a4d43be42cbf4d243b0fdd0
Russell <russell.w.bentley@icloud.com>
2025-06-12 15:08:32 -0400

    Add timers to heat examples, add rotating tv example (#65)
    * Add timers to heat examples, add rotating tv example
    
    * actually add tv_rotating_2d

--- examples/tv_rotating_2d.rs
+++ examples/tv_rotating_2d.rs
@@ -1,34 +1,30 @@
+use core::f64;
+
 use nhls::ap_solver::*;
 use nhls::domain::*;
-use nhls::fft_solver::DirectFrustrumSolver;
-use nhls::image_3d_example::*;
-use nhls::vtk::*;
+use nhls::image::*;
+use nhls::image_2d_example::*;
+use nhls::init::*;
 use std::time::*;
 
 fn main() {
-    let args = Args::cli_parse("heat_3d_ap_fft");
+    let args = Args::cli_parse("tv_rotating_2d");
 
-    let stencil =
-        nhls::standard_stencils::heat_3d(1.0, 1.0, 1.0, 1.0, 0.1, 0.1, 0.1);
-
+    // Grid size
     let grid_bound = args.grid_bounds();
+
+    let freq = (2.0 * std::f64::consts::PI) / 1500.0;
+    let stencil =
+        nhls::standard_stencils::RotatingAdvectionStencil::new(freq, 0.2);
 
     // Create domains
     let mut buffer_1 = OwnedDomain::new(grid_bound);
     let mut buffer_2 = OwnedDomain::new(grid_bound);
     let mut input_domain = buffer_1.as_slice_domain();
     let mut output_domain = buffer_2.as_slice_domain();
+    normal_ic_2d(&mut input_domain, args.chunk_size);
 
-    // Create BC
-    let bc = ConstantCheck::new(1.0, grid_bound);
-
-    let direct_solver = DirectFrustrumSolver {
-        bc: &bc,
-        stencil: &stencil,
-        stencil_slopes: stencil.slopes(),
-        chunk_size: args.chunk_size,
-    };
-
+    let direct_solver = DirectSolver5Pt2DOpt::new(&stencil);
     // Create AP Solver
     let planner_params = PlannerParameters {
         plan_type: args.plan_type,
@@ -40,21 +36,18 @@ fn main() {
         aabb: grid_bound,
     };
     let mut solver =
-        generate_ap_solver(&stencil, direct_solver, &planner_params);
+        generate_tv_ap_solver(&stencil, direct_solver, &planner_params);
 
-    solver.print_report();
-    if args.write_dot {
-        solver.to_dot_file(&args.dot_path());
-    }
     if args.gen_only {
         args.save_wisdom();
         std::process::exit(0);
     }
 
     if args.write_images {
-        write_vtk3d(&input_domain, &args.frame_name(0));
+        image2d(&input_domain, &args.frame_name(0));
     }
 
+    // Apply direct solver
     let mut global_time = 0;
     for t in 1..args.images {
         let now = Instant::now();
@@ -65,7 +58,7 @@ fn main() {
         global_time += args.steps_per_image;
         std::mem::swap(&mut input_domain, &mut output_domain);
         if args.write_images {
-            write_vtk3d(&input_domain, &args.frame_name(t));
+            image2d(&input_domain, &args.frame_name(t));
         }
     }
 
