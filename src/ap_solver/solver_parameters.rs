a17252aaa60973d610027d6c6c89371044b936d5
RussellBentley <russell.w.bentley@icloud.com>
2025-06-14 20:52:57 -0400

    Add docs

--- src/ap_solver/solver_parameters.rs
+++ src/ap_solver/solver_parameters.rs
@@ -1,14 +1,43 @@
 use crate::fft_solver::PlanType;
 use crate::util::*;
 
-/// Planner recurses by finding periodic solves.
-/// These solves are configured with these parameters.
+/// Solver generation is configurable.
+/// These are all the parameters.
 pub struct SolverParameters<const GRID_DIMENSION: usize> {
+    /// Number of steps for one `apply` operation.
     pub steps: usize,
+
+    /// Optimization level for FFTW3 plans.
     pub plan_type: PlanType,
+
+    /// Per axis cutoff for applying periodic solves.
     pub cutoff: i32,
+
+    /// Approximate output size for periodic solves.
     pub ratio: f64,
+
+    /// Some multithreaded operations on vectors are chunked with this size
     pub chunk_size: usize,
+
+    /// How many threads are available for `apply` operatons.
     pub threads: usize,
+
+    /// Domain bounds
     pub aabb: AABB<GRID_DIMENSION>,
+}
+
+impl<const GRID_DIMENSION: usize> std::default::Default
+    for SolverParameters<GRID_DIMENSION>
+{
+    fn default() -> Self {
+        SolverParameters {
+            steps: 100,
+            plan_type: PlanType::Estimate,
+            cutoff: 100,
+            ratio: 0.5,
+            chunk_size: 1000,
+            threads: 1,
+            aabb: AABB::new(Bounds::zeros()),
+        }
+    }
 }
