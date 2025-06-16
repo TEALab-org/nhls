01ca2f2670ad23eead778feab17df94e183f02dd
RussellBentley <russell.w.bentley@icloud.com>
2025-06-16 10:43:39 -0400

    Fix more examples

--- examples/heat_1d_ap_direct.rs
+++ examples/heat_1d_ap_direct.rs
@@ -1,4 +1,3 @@
-/*
 use nhls::domain::*;
 use nhls::image_1d_example::*;
 use nhls::solver::*;
@@ -9,37 +8,15 @@ fn main() {
     let stencil = nhls::standard_stencils::heat_1d(1.0, 1.0, 0.5);
 
     // Create BC
+    let grid_bound = args.grid_bounds();
     let bc = ConstantCheck::new(1.0, grid_bound);
 
-    // Make image
-    let mut img = None;
-    if args.write_image {
-        let mut i = nhls::image::Image1D::new(grid_bound, args.lines as u32);
-        i.add_line(0, input_domain.buffer());
-        img = Some(i);
-    }
+    let mut solver = GeneralDirectBoxSolver::new(
+        &bc,
+        &stencil,
+        args.steps_per_line,
+        args.chunk_size,
+    );
 
-    let mut global_time = 0;
-    for t in 1..args.lines as u32 {
-        box_apply(
-            &bc,
-            &stencil,
-            &mut input_domain,
-            &mut output_domain,
-            args.steps_per_line,
-            global_time,
-            args.chunk_size,
-        );
-        global_time += args.steps_per_line;
-        std::mem::swap(&mut input_domain, &mut output_domain);
-        if let Some(i) = img.as_mut() {
-            i.add_line(t, input_domain.buffer());
-        }
-    }
-    if let Some(i) = img {
-        i.write(&output_image_path.unwrap());
-    }
-
-    args.finish();
+    args.run_solver(&mut solver);
 }
-*/
--- examples/tv_rotating_2d.rs
+++ examples/tv_rotating_2d.rs
@@ -1,28 +1,12 @@
-use core::f64;
-
 use nhls::ap_solver::*;
-use nhls::domain::*;
-use nhls::image::*;
 use nhls::image_2d_example::*;
-use nhls::init::*;
-use std::time::*;
 
 fn main() {
     let args = Args::cli_setup("tv_rotating_2d");
 
-    // Grid size
-    let grid_bound = args.grid_bounds();
-
     let freq = (2.0 * std::f64::consts::PI) / 1500.0;
     let stencil =
         nhls::standard_stencils::RotatingAdvectionStencil::new(freq, 0.2);
-
-    // Create domains
-    let mut buffer_1 = OwnedDomain::new(grid_bound);
-    let mut buffer_2 = OwnedDomain::new(grid_bound);
-    let mut input_domain = buffer_1.as_slice_domain();
-    let mut output_domain = buffer_2.as_slice_domain();
-    normal_ic_2d(&mut input_domain, args.chunk_size);
 
     let direct_solver = DirectSolver5Pt2DOpt::new(&stencil);
 
@@ -31,29 +15,5 @@ fn main() {
     let mut solver =
         generate_tv_ap_solver(&stencil, direct_solver, &solver_params);
 
-    if args.gen_only {
-        args.finish();
-        std::process::exit(0);
-    }
-
-    if args.write_images {
-        image2d(&input_domain, &args.frame_name(0));
-    }
-
-    // Apply direct solver
-    let mut global_time = 0;
-    for t in 1..args.images {
-        let now = Instant::now();
-        solver.apply(&mut input_domain, &mut output_domain, global_time);
-        let elapsed_time = now.elapsed();
-        eprintln!("{}", elapsed_time.as_nanos() as f64 / 1000000000.0);
-
-        global_time += args.steps_per_image;
-        std::mem::swap(&mut input_domain, &mut output_domain);
-        if args.write_images {
-            image2d(&input_domain, &args.frame_name(t));
-        }
-    }
-
-    args.finish();
+    args.run_solver(&mut solver);
 }
